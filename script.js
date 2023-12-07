087e5612bba19a9597f9214cfd8ba6f6e03e0c7d
HARSHIT MISHRA <harshitmishra1255@gmail.com>
2023-12-07 22:19:55 +0530

    mobile view added

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log('hello world');
+let menu = document.getElementById('menu')
+let menustate = false;
+menu.addEventListener('click',()=>
+{   
+    if(menustate===false)
+    {
+        document.querySelector('nav').style.display='block'
+        menustate=true;
+    }
+    else
+    {
+        document.querySelector('nav').style.display='none'
+        menustate=false}
+})
